e620d49909089631a3817596b2b526e58c6b9d5f
MUKIVA <mukiva2261@gmail.com>
2022-11-16 21:27:16 +0300

    Сделал обработку уведомлений

--- CrazyPoleMobile/Platforms/Android/MainActivity.cs
+++ CrazyPoleMobile/Platforms/Android/MainActivity.cs
@@ -7,5 +7,26 @@ namespace CrazyPoleMobile;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    
+    public const string CHANNEL_ID = "PUSH_NOTIFY_CHANNEL";
+    public const int NOTIFICATION_ID = 100;
+
+    private NotificationManager _notificationManager;
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        _notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+        CreateNotificationChannel();
+
+    }
+
+    private void CreateNotificationChannel()
+    {
+        if (OperatingSystem.IsOSPlatformVersionAtLeast("android", 26))
+        {
+            var channel = new NotificationChannel(CHANNEL_ID, "Push Notify Channel", NotificationImportance.Default);
+            _notificationManager.CreateNotificationChannel(channel);
+        }
+    }
 }
---
+++ CrazyPoleMobile/Platforms/Android/Services/FirebaseService.cs
@@ -0,0 +1,60 @@
+﻿
+using Android.App;
+using Android.Content;
+using AndroidX.Core.App;
+using Firebase.Messaging;
+
+namespace CrazyPoleMobile.Platforms.Android.Services
+{
+    [Service(Exported = true)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseService : FirebaseMessagingService
+    {
+        public const string DEVICE_TOKEN_NAME = "DeviceToken";
+
+        public FirebaseService() { }
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+            if (Preferences.ContainsKey(DEVICE_TOKEN_NAME))
+            {
+                Preferences.Remove(DEVICE_TOKEN_NAME);
+            }
+
+            Preferences.Set(DEVICE_TOKEN_NAME, token);
+        }
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+
+            var notification = message.GetNotification();
+
+            SendNotification(notification.Body, notification.Title, message.Data);
+        }
+
+        private void SendNotification(string messageBody, string title, IDictionary<string, string> data) 
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+
+            foreach (var key in data.Keys) 
+            {
+                intent.PutExtra(key, data[key]);
+            }
+
+            var pendingIntent = PendingIntent.GetActivity(this,
+                MainActivity.NOTIFICATION_ID, intent, PendingIntentFlags.Immutable);
+
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                .SetContentTitle(title)
+                .SetSmallIcon(Resource.Mipmap.appicon)
+                .SetContentText(messageBody)
+                .SetChannelId(MainActivity.CHANNEL_ID)
+                .SetContentIntent(pendingIntent)
+                .SetPriority(2);
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(MainActivity.NOTIFICATION_ID, notificationBuilder.Build());
+        }
+    }
+}
