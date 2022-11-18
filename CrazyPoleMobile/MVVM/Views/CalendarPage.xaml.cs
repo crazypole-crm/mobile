e60fe60fe641406843921533a8716e1fc859fdc2
MUKIVA <mukiva2261@gmail.com>
2022-11-18 14:52:12 +0300

    Merge remote-tracking branch 'origin/devUI' into dev
    # Conflicts:
    #	.vs/CrazyPoleMobile/v17/.suo
    #	CrazyPoleMobile/CrazyPoleMobile.csproj
    #	CrazyPoleMobile/CrazyPoleMobile.csproj.user
    #	CrazyPoleMobile/MVVM/Models/TrainingData.cs
    #	CrazyPoleMobile/MVVM/ViewModels/CalendarPageViewModel.cs
    #	CrazyPoleMobile/MVVM/Views/CalendarPage.xaml.cs
    #	CrazyPoleMobile/MVVM/Views/NotificationPage.xaml.cs
    #	CrazyPoleMobile/Services/PageNavigationService.cs

--- CrazyPoleMobile/MVVM/Views/CalendarPage.xaml.cs
+++ CrazyPoleMobile/MVVM/Views/CalendarPage.xaml.cs
@@ -2,14 +2,9 @@ namespace CrazyPoleMobile.MVVM.Views;
 
 public partial class CalendarPage : ContentPage
 {
-<<<<<<< Updated upstream
-	public CalendarPage()
-	{
-=======
 	public CalendarPage(CalendarPageViewModel viewmodel)
 	{
 		BindingContext = viewmodel;
->>>>>>> Stashed changes
 		InitializeComponent();
 	}
 }
