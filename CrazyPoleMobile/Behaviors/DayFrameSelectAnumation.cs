7e7c78acf65379b0214eaec5f298bfd9e7219cb0
MUKIVA <mukiva2261@gmail.com>
2022-11-27 17:02:34 +0300

    Добавлен макет страницы хоум

---
+++ CrazyPoleMobile/Behaviors/DayFrameSelectAnumation.cs
@@ -0,0 +1,39 @@
+﻿using CrazyPoleMobile.MVVM.Views.Components;
+
+namespace CrazyPoleMobile.Behaviors
+{
+    public class DayFrameSelectAnumation : Behavior<CalendarDayFrame>
+    {
+
+        private CalendarDayFrame _obj;
+
+        public static readonly BindableProperty IsSelectProperty =
+        BindableProperty.Create(nameof(IsSelect), typeof(bool), typeof(CalendarDayFrame), true);
+
+        public bool IsSelect
+        {
+            get => (bool)GetValue(IsSelectProperty);
+            set
+            {
+                SetValue(IsSelectProperty, value);
+                HandleSelectionChanged(value);
+            }
+        }
+
+        protected override void OnAttachedTo(CalendarDayFrame bindable)
+        {
+            base.OnAttachedTo(bindable);
+            _obj = bindable;
+        }
+
+        private async void HandleSelectionChanged(bool value)
+        {
+            if (value)
+                await _obj.TranslateTo(0, -5);
+
+            if (!value)
+                await _obj.TranslateTo(0, 0);
+        }
+
+    }
+}
---
+++ CrazyPoleMobile/Services/Api/ITrainingsApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyPoleMobile.Services.Api
+{
+    public interface ITrainingsApi
+    {
+        public Task<HttpStatusCode> GetTrainingsList();
+    }
+}
