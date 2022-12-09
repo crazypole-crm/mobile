5de9228d6692f462756ec42172e75356a99088de
Koneist <61579930+Koneist@users.noreply.github.com>
2022-12-09 16:58:54 +0300

    Add FilterService

---
+++ CrazyPoleMobile/Services/IFilterService.cs
@@ -0,0 +1,19 @@
+﻿using CrazyPoleMobile.Services.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyPoleMobile.Services
+{
+    public interface IFilterService<T>
+    {
+        public IEnumerable<T> Filtrate(IEnumerable<T> collection);
+        public void AddFilter(IFilterElement<T> filterElement);
+        public void AddFilters(IEnumerable<IFilterElement<T>> filterCollection);
+        public void RemoveFilter(IFilterElement<T> filterElement);
+        public void ClearFilters();
+
+    }
+}
---
+++ CrazyPoleMobile/Services/TrainingFilterService.cs
@@ -0,0 +1,51 @@
+﻿using CrazyPoleMobile.MVVM.Models;
+using CrazyPoleMobile.Services.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyPoleMobile.Services
+{
+    public class TrainingFilterService : IFilterService<TrainingData>
+    {
+        private List<IFilterElement<TrainingData>> _filters;
+
+        public IEnumerable<TrainingData> Filtrate(IEnumerable<TrainingData> collection)
+        {
+            return collection.Where(MatchFilters);
+        }
+
+        private bool MatchFilters(TrainingData training)
+        {
+            foreach(var filter in _filters)
+                if(!filter.Match(training))
+                    return false;
+            
+            return true;
+        }
+
+        public void AddFilter(IFilterElement<TrainingData> filterElement)
+        {
+            _filters.Add(filterElement);
+        }
+
+        public void AddFilters(IEnumerable<IFilterElement<TrainingData>> filterCollection)
+        {
+            _filters.AddRange(filterCollection);
+        }
+
+        public void RemoveFilter(IFilterElement<TrainingData> filterElement)
+        {
+            _filters.Remove(filterElement);
+        }
+
+        public void ClearFilters()
+        {
+            _filters.Clear();
+        }
+
+         
+    }
+}
